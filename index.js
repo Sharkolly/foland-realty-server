8ce4147c073d7e5a6428eea441576199e7f23728
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-02-20 11:57:22 +0100

    new cors

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ const mongoDBURL = process.env.MONGODBURL;
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://foland-realty.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
+
+app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://foland-realty.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(bodyParser());
 app.use(cookieParser());
 
