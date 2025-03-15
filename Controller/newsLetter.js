df26a1f344d74371c9970c4210c737783d3d7abf
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-03-15 08:24:21 +0100

    new commit

--- Controller/newsLetter.js
+++ Controller/newsLetter.js
@@ -2,8 +2,8 @@ const NewsLetter = require("../Models/NewsLetter");
 
 const newsLetter = async (req, res) => {
   const { email } = req.body;
-  const regexForValidEmail = /^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-  console.log(email)
+  const regexForValidEmail = /^(?!.*\.\.)[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
   if (!email) {
     return res.status(403).json({ message: "Enter email address!" });
   }
