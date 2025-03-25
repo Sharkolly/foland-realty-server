fbdb8f740f31b7a18f8e167d2755cdc4819af641
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-03-25 15:48:28 +0100

    new updates

--- Controller/getUserSavedProperties.controller.js
+++ Controller/getUserSavedProperties.controller.js
@@ -1,13 +1,14 @@
 import { getUserSavedProperties } from "../mongodb/controller/saveProperty.model.js";
 
 const getAllSavedPropertiesController = async (req, res) => {
-  const { user } = req;  
+  const { user } = req;
   try {
- 
-    const userSavedProperties = await getUserSavedProperties(user);    
+    const userSavedProperties = await getUserSavedProperties(user);
     res.status(200).json({ userSavedProperties });
   } catch (err) {
-    res.status(500).json({ message: "Internal server error", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: err.message });
   }
 };
 export default getAllSavedPropertiesController;
