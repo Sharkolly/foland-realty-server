95604c7b19856ea5e889dacfe5ee2d8d434eb84e
Sharkolly <sharkollymofeoluwa@gmail.com>
2025-06-01 01:34:20 +0100

    new url

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "http://foland-realty-nextjs.vercel.app",
+      "https://foland-realty-nextjs.vercel.app",
       "http://localhost:5174",
       // "https://practice-socketio.vercel.app",
       "https://foland-realty.vercel.app",
